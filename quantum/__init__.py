eb46c10357fe885edad3fb7eb8d0c983c6f23e1d
monisha-eapu <monishaeapu@gmail.com>
2026-09-09 21:37:58 +0530

    Export QRBM helper functions

--- quantum/__init__.py
+++ quantum/__init__.py
@@ -1,1 +1,13 @@
-from .qrbm import train_qrbm, predict_qrbm
+from .qrbm import (
+    compute_auprc,
+    compute_false_negative_rate,
+    predict_qrbm,
+    train_qrbm,
+)
+
+__all__ = [
+    "train_qrbm",
+    "predict_qrbm",
+    "compute_auprc",
+    "compute_false_negative_rate",
+]
